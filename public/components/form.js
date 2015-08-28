dbb6a99f3b4fa2d7f73bc70c4b0af84178d5df1d
TPewarski <tjp2122@gmail.com>
2015-08-28 14:24:53 -0400

    installs webpack

--- public/components/form.js
+++ public/components/form.js
@@ -1,266 +1,240 @@
-var DibsForm = React.createClass({
-  getInitialState: function(){
-    return {
-      "isLoading": true,
-      "itemEnums": {
-        material: [],
-        measurement: {
-          unit: {},
-          shape: []
-        },
-        condition: {
-          description: []
-        } 
-      },
-      req: {
-    "httpCode": 0,
-    "message": "",
-    "result": {
-        "item": {
-            "id": 0,
-            "title": "",
-            "description": "",
-            "dealerInternalNotes": "",
-            "material": {
-                "description": "",
-                "restricted": ""
-            },
-            "measurement": {
-                "unit": "",
-                "shape": "",
-                "length": "",
-                "depth": "",
-                "height": ""
-            },
-            "condition": {
-                "description": ""
-            }
-        }
-    }
-}
-    };
-  },
+var ItemForm = React.createClass({
+	getInitialState: function(){
+		return {
+			isLoading: true
+		}
+	},
+	componentDidMount: function(){
+	    var self = this;
+	    var enumsDone = false
 
-  componentDidMount: function(){
-    //fetch to get enumerables
-    var self = this;
-    var enumsDone = false
+	    Promise.all([
+	      fetch('json/enums.json').then(function(resp){
+	        return resp.json()
+	      }).then(function(data){
+	        self.setState(data)
+	      }),
+	      fetch('json/item.json').then(function(resp){
+	        return resp.json()
+	      }).then(function(data){
+	        self.setState({req: data.result.item})
+	      })
+	    ]).then(function () {
+	      self.setState({isLoading: false})
+	    })
+	},
+	toggleCheck: function(){
+	    if(this.state.req.material.restricted == "N"){
+	      this.state.req.material.restricted = "Y";
+	    }else{
+	      this.state.req.material.restricted = "N"
+	    }
+    	console.log("Checkbox state", this.state.req.material.restricted)
+  	},
+	updateReq: function(){
+	    var reqObj = this.state.req
+	    var name = event.target.name
+	    var period = name.indexOf(".")
 
-    Promise.all([
-      fetch('json/enums.json').then(function(resp){
-        return resp.json()
-      }).then(function(data){
-        self.setState(data)
-        //console.log("self.state", self.state)
-      }),
-      fetch('json/item.json').then(function(resp){
-        return resp.json()
-      }).then(function(data){
-        self.setState({req: data})
-      })
-    ]).then(function () {
-      self.setState({isLoading: false});
-    })
-  },
-  updateReq: function(){
-    var reqObj = this.state.req.result.item
-    var name = event.target.name
-    var period = name.indexOf(".")
-    if(period > -1){
-      //console.log("name2", name.slice(period+1) )
-      //console.log("sliced val", reqObj[name.slice(0, period)][name.slice(period+1)])
-      reqObj[name.slice(0, period)][name.slice(period+1)] = event.target.value
-    }else{
-      reqObj[event.target.name] = event.target.value
-    }
-    this.setState({})
-    console.log("reqObj", reqObj)
-    console.log("total state", this.state)
-  },
-  toggleCheck: function(){
-    if(this.state.req.result.item.material.restricted == "N"){
-      this.state.req.result.item.material.restricted = "Y";
-    }else{
-      this.state.req.result.item.material.restricted = "N"
-    }
-    //console.log("Checkbox state", this.state.req.result.item.material.restricted)
-  },
-  render: function() {
-    //console.log(this.state.req.result.item.title)
-    if(this.state.isLoading){
-      return <h1>Loading</h1>
-    }else{
+	    if(period > -1){
+	      reqObj[name.slice(0, period)][name.slice(period+1)] = event.target.value
+	    }else{
+	      reqObj[event.target.name] = event.target.value
+	    }
 
-      var title = this.state.req.result.item.title
-      var description = this.state.req.result.item.description
-      var internalNotes = this.state.req.result.item.dealerInternalNotes
-      var material = this.state.req.result.item.material
-      var condition = this.state.req.result.item.condition.description
-      return (
-        <form className="dibsForm">
-         
-          <h3>Title</h3>
-          <input value={title} onChange={this.updateReq} type="text" name="title"></input>
-          <TextArea state={description} name="description" onChange={this.updateReq} title="Description" rows="10" cols="50" />
-          <TextArea state={internalNotes} name="dealerInternalNotes" onChange={this.updateReq} title="Internal Notes" rows="3" cols="50" />
-          <Materials state={material.description} onChange={this.updateReq} materials={this.state.itemEnums.material} />
-          <br />
-          <CheckBox state={material.restricted} name="material.restricted" onChange={this.toggleCheck} /><b>Check this box</b> if the listing contains or may contain restricted material
-          { /*console.log("this.state.measurement.unit", this.state.measurement.unit)*/}
-          <ShapeDetails state={this.state.req.result.item.measurement} onChange={this.updateReq} units={this.state.itemEnums.measurement.unit} />
-          <Condition state={condition} onChange={this.updateReq} types={this.state.itemEnums.condition.description} />
-        </form>
-      );
-    }
-    }
-});
-
-
-var TextArea = React.createClass({
-  render: function(){
-
-    return (
-      <div>
-        <h3>{this.props.title}</h3>
-        <textarea value={this.props.state} name={this.props.name} onChange={this.props.onChange} rows={this.props.rows} cols={this.props.cols}></textarea>
-      </div>
-      );
-  }
-});
-
-var CheckBox = React.createClass({
-  render: function(){
-    return (
-      <input checked={this.props.state === "Y" ? true: false} name={this.props.name} value={this.props.name} onChange={this.props.onChange} type="checkbox" />
-      )
-  }
-});
-
-var RadioBtn = React.createClass({
-  render: function(){
-    // console.log("RDBTN state", this.props.state)
-    // console.log("RDBTN val", this.props.val)
-    // console.log("match", this.props.state == this.props.val ? "true" : "false" )
-    return (
-      <div>
-        <input checked={this.props.state == this.props.val ? "true" : false} type="radio" name={this.props.name} value={this.props.val} onChange={this.props.onChange} />
-        {this.props.data}
-      </div>
-      )
-  }
+	    console.log("reqObj", reqObj)
+	    console.log("total state", this.state)
+	    this.setState({})
+  	},	
+	render: function(){
+		if(this.state.isLoading){
+			return <h1>Loading</h1>
+		}else{
+			var title = this.state.req.title
+			var description = this.state.req.description;
+			var internalDealerNotes = this.state.req.dealerInternalNotes;
+			var materialEnums = this.state.itemEnums.material
+			var material = this.state.req.material
+			var measurements = this.state.req.measurement
+			var measurementEnums = this.state.itemEnums.measurement
+			var condition = this.state.req.condition.description
+			var conditionEnums = this.state.itemEnums.condition.description
+			return (
+				<form className="col-md-8">
+					<div className="form-group">
+						<h1>Item Form</h1>
+						<Title value={title} onChange={this.updateReq} />
+						<Description value={description} onChange={this.updateReq} />
+						<InternalNotes value={internalDealerNotes} onChange={this.updateReq} />
+						<Materials value={material} onChange={this.updateReq} enums={materialEnums} toggleCheck={this.toggleCheck} />
+						<Measurements value={measurements} onChange={this.updateReq} enums={measurementEnums} />
+						<Condition value={condition} onChange={this.updateReq} enums={conditionEnums} />
+					</div>
+					<button style={{margin: "10px"}} className="btn btn-primary">Save!</button>
+				</form>
+			)
+		}
+	}
 })
 
 
 
-var InputText = React.createClass({
-  render: function(){
-    //console.log("this.props.disabled", this.props.disabled)
-    return (
+var Header = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h3><b>{this.props.bold}</b>{this.props.normal}</h3>
+			</div>
+			)
+	}
+})
+var Selector = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<select style={{width: "50%"}} className="form-control" value={this.props.value} name={this.props.name} onChange={this.props.onChange}>
+					{this.props.enums.map(function(option){
+						return <option value={option}>{option}</option>
+					})}
+				</select>
+			</div>
+		)
+	}
+})
 
-      <div>
-        <h3>{this.props.title}</h3>
-        <input value={this.props.value} onChange={this.props.onChange} name={this.props.name} type="text" disabled={this.props.disabled}/>
-      </div>
-    )
-  }
+var RadioGroup = React.createClass(({
+	render: function(){
+		var self = this
+		console.log("this.props.enums", this.props.enums)
+		return(
+			<span>
+				{self.props.enums.map(function(option){
+					return (
+						<span style={{padding: "5px"}}>
+							<input style={{margin: "5px"}} type="radio" name={self.props.name} onChange={self.props.onChange} value={option} checked={self.props.checked == option ? true : false} />
+							<span>{option}</span>
+						</span>
+					)
+				})}
+			</span>
+		)
+	}
+}))
+
+var Title = React.createClass({
+	render: function(){
+		return (
+			<div className="col-md-12">
+				<Header bold="Title" />
+				<input className="form-control" value={this.props.value} onChange={this.props.onChange} type="text" name="title" />
+			</div>
+		)
+	}
+})
+
+var Description = React.createClass({
+	render: function(){
+		console.log("description value", this.props.value)
+		return (
+			<div className="col-md-12">
+				<Header bold="Description" />
+				<textarea className="form-control" value={this.props.value} onChange={this.props.onChange} name="description" rows="10" />
+			</div>
+		)
+	}
+})
+
+var InternalNotes = React.createClass({
+	render: function(){
+		return (
+			<div className="col-md-12">
+				<Header bold="Internal Notes" />
+				<textarea className="form-control" value={this.props.value} onChange={this.props.onChange} name="dealerInternalNotes" rows="4" />
+			</div>
+		)
+	}
 })
 
 var Materials = React.createClass({
-  render: function(){
-    var self = this;
-    return(
-      <div>
-        <h3>Materials</h3>
-        {this.props.materials}
-        <select value={this.props.state} name="material.description" onChange={this.props.onChange}>
-          
-          {this.props.materials.map(function(material){
-            return <option value={material}>{material}</option>
-          })}
-        </select>
-      </div>
-    )
-  }
-})
-
-var InputSpecs = React.createClass({
-  render: function(){
-    //console.log("this.props.shape", this.props.shape)
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+	render: function(){
+		return (
+			<div className="col-md-12">
+				<Header bold="Materials" />
+				<Selector value={this.props.value.description} enums={this.props.enums} onChange={this.props.onChange} name="material.description" />
+				<input type="checkBox" value={this.props.value.restricted} onChange={this.props.toggleCheck} />
+				<span><b> Check this box</b> if the listing contains or may contain restricted materials</span>
+			</div>
+		)
+	}
 })
 
 var Measurements = React.createClass({
-  render: function(){
-    //console.log("this.props.units.in", this.props.units)
-    return (
-      <div>
-        <br />
-        <b>Measurements</b>
-        <div>
-          <br />
-          <span>Measurements are in: </span>
-            <RadioBtn state={this.props.state.unit} val="in" onChange={this.props.onChange} data="Inches (in) " name="measurement.unit" />
-            <RadioBtn state={this.props.state.unit }val="cm" onChange={this.props.onChange} data="Centimeters (cm)" name="measurement.unit"/>
-        </div>
-        <div eventCB={this.props.eventCB}>
-          <span>Measured Item is: </span>
-          
-          <RadioBtn onChange={this.props.onChange} state={this.props.state.shape} val="rectangle" data="Rectangular " name="measurement.shape" />
-          <RadioBtn onChange={this.props.onChange} state={this.props.state.shape} val="circle" data="Circular" name="measurement.shape"/>
-        </div>
-      </div>
-      )
-  }
-})
-
-var ShapeDetails = React.createClass({
-  getInitialState: function(){
+	getInitialState: function(){
     return {
-      shape: 'circle'
+      shape: this.props.value.shape,
+      unit: this.props.value.unit
     }
   },
   handleChange: function(){
-    //console.log("hit handler")
     this.props.onChange()
-    this.setState({shape: event.target.value})
+    if(event.target.name == "measurement.unit"){
+    	this.setState({unit: event.target.value})
+    }else{
+	    this.setState({shape: event.target.value})
+    }
   },
   render: function(){
-    //console.log("state shape", this.state.shape)
-    return (
-      <div>
-       
-        <Measurements state={this.props.state} onChange={this.handleChange} units={this.props.units} />
-        <InputSpecs state={this.props.state} onChange={this.props.onChange} shape={this.state.shape} />
-      </div>
-      )
+  	var self = this;
+  	return (
+  		<div className="col-md-12">
+	  		<Header bold="Measurements" />
+	  		<p>Measurements are in: 
+		  		<input style={{margin: "5px"}} type="radio" checked={this.props.value.unit == "in" ? true : false} name="measurement.unit" value="in" onChange={this.handleChange} />
+		  		<span> Inches(in) </span>
+		  		<input style={{margin: "5px"}} type="radio" checked={this.props.value.unit == "cm" ? true : false} name="measurement.unit" value="cm" onChange={this.handleChange} />
+		  		<span> Centimeters(cm) </span>
+		  	</p>
+		  	<p>Measured item is: 
+		  		<RadioGroup enums={this.props.enums.shape} name="measurement.shape" onChange={this.handleChange} checked={this.state.shape} />
+		  	</p>
+	  		<div className="col-md-6">
+	  		  <Header bold="Length" />
+	  		  <span className="input-group">
+		          <input className="form-control" value={this.props.value.length} onChange={this.props.onChange} name="measurement.length" disabled={this.state.shape == "Rectangular" ? false : true} /><span className="input-group-addon">{this.state.unit}</span>
+	          </span>
+	          <Header bold="Depth" />
+	          <span className="input-group"> 
+		          <input className="form-control" value={this.props.value.depth} onChange={this.props.onChange} name="measurement.depth" disabled={this.state.shape == "Rectangular" ? false : true} /><span className="input-group-addon">{this.state.unit}</span>
+	          </span>
+	        </div>
+	        <div className="col-md-6" >
+	          <Header bold="Height" />
+	          <span className="input-group">
+		          <input className="form-control" value={this.props.value.height} onChange={this.props.onChange} name="measurement.height" disabled={this.state.shape == "Rectangular" ? false : true} /><span className="input-group-addon">{this.state.unit}</span>
+	          </span>
+        	  <Header bold="Diameter" /> 
+        	  <span className="input-group">
+		          <input className="form-control" value={this.props.value.diameter} onChange={this.props.onChange} name="measurement.diameter" disabled={this.state.shape == "Circular" ? false : true} /><span className="input-group-addon">{this.state.unit}</span>
+	          </span>
+	        </div>
+	  	</div>
+  	)
   }
 })
 
 var Condition = React.createClass({
-  render: function(){
-    var self=this
-    return (
-      <div>
-        <h3>Condition</h3>
-
-        {this.props.types.map(function(type, i){
-          //console.log("type", type)
-          return <RadioBtn state={self.props.state} onChange={self.props.onChange} val={type} data={type} key={i} name="condition.description" />
-          {/*return <input type="radio" name="condition" value="type" key={i}>{type}</input>*/}
-        })}
-      </div>
-
-    )
-  }
+	render: function(){
+		return(
+			<div className="col-md-12">
+				<Header bold="Condtion " normal="(Select one)" />
+				<RadioGroup enums={this.props.enums} name="condition.description" onChange={this.props.onChange} checked={this.props.value} />
+			</div>
+		)
+	}
 })
 
 
 React.render(
-  <DibsForm />,
-  document.getElementById('content')
-);
+	<ItemForm />,
+	document.getElementById('content2')
+	)
